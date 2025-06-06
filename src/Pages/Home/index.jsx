cf4724aa526f0632886576fe69b44007aff88d70
Njaka02 <njakaharilantoandriamijeri@gmail.com>
2025-06-06 15:01:40 +0300

    [ADD] Add framer motion

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,18 +1,23 @@
+import { ArrowDown } from "lucide-react";
 import { motion as Motion } from "framer-motion";
 import home1 from "../../assets/image/BgLogin1.jpeg";
 import NTLogo from "../../assets/image/N_TLogo-removebg-preview.png";
+import Chambre from "../../assets/image/ChambreIcon.png";
+import Restaurent from "../../assets/image/RestaurentIcon.png";
+import Bar from "../../assets/image/BarIcon.png";
+import SPA from "../../assets/image/SpaIcon.png";
 
 function Home() {
+  const decouvrHotel = [
+    { name: "Chambre", path: "/", logo: <img src={Chambre} /> },
+    { name: "Restaurent", path: "/", logo: <img src={Restaurent} /> },
+    { name: "Bar", path: "/", logo: <img src={Bar} /> },
+    { name: "SPA", path: "/", logo: <img src={SPA} /> },
+  ];
   return (
     <>
       <div>
-        <div>
-          <h1 className="text-4xl text-white bg-amber-500">
-            Bienvenue Sur Kambana Hotel
-          </h1>
-        </div>
-
-        <div className="relative  overflow-hidden">
+        <div className="relative overflow-hidden">
           <Motion.div
             initial={{ scale: 1.1 }}
             animate={{ scale: 1 }}
@@ -38,19 +43,54 @@ function Home() {
             />
           </Motion.div>
           <Motion.div
-            initial={{ y: 30, opacity: 0 }}
-            animate={{ y: 0, opacity: 1 }}
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
             transition={{
               type: "spring",
               stiffness: 50,
-              delay: 0.6,
+              delay: 1,
             }}
-            className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-blue-950 font-bold text-5xl"
+            className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-teal-900 font-bold text-5xl"
           >
             <h1 className="text-3xl md:text-5xl lg:text-6xl font-bold pt-20">
               Bienvenue au N&T Hotel
             </h1>
+            <Motion.p
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              transition={{ delay: 1.5 }}
+              className="mt-2 text-2xl md:text-xl text-teal-900 font-medium"
+            >
+              Votre séjour d'exception
+            </Motion.p>
           </Motion.div>
+
+          <Motion.div
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{
+              type: "spring",
+              stiffness: 50,
+              delay: 2,
+            }}
+            className="absolute bottom-10 left-1/2 -translate-x-1/2 -translate-y-1/2 text-center animate-bounce"
+          >
+            <p className="text-teal-900 uppercase font-bold">Découvrir</p>
+            <div className="inline-flex border-2 text-teal-900 p-3 rounded-full mt-5 animate-bounce">
+              <ArrowDown size={30} className="text-teal-900" />
+            </div>
+          </Motion.div>
+        </div>
+
+        <div className="flex ">
+          {decouvrHotel.map((dHotel, index) => (
+            <li key={index} className="list-none">
+              <button className="w-20 h-20">
+                <span>{dHotel.logo}</span>
+                <span> {dHotel.name}</span>
+              </button>
+            </li>
+          ))}
         </div>
       </div>
     </>
