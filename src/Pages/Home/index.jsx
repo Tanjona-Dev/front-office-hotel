769aefba06ea5a6df5019d2dab1b2b04a0fd3f4c
Njaka02 <njakaharilantoandriamijeri@gmail.com>
2025-06-11 20:28:02 +0300

    [ADD] add responsive designe

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,11 +1,12 @@
 import Avantage from "./avantages";
+import OurServices from "./ourService";
 import { ArrowDown } from "lucide-react";
 import TypeDeChambre from "./typeDeChambre";
 import Bar from "../../assets/image/bar.png";
 import SPA from "../../assets/image/spa.png";
 import { motion as Motion } from "framer-motion";
-import Chambre from "../../assets/image/hotel.png";
 import home1 from "../../assets/image/Login.jpg";
+import Chambre from "../../assets/image/hotel.png";
 import Restaurent from "../../assets/image/restaurant.png";
 import NTLogo from "../../assets/image/N_TLogo-removebg-preview.png";
 
@@ -56,16 +57,16 @@ function Home() {
               stiffness: 50,
               delay: 1,
             }}
-            className="absolute top-1/3 left-1/2 transform -translate-x-1/2 -translate-y-1/2 font-bold text-5xl"
+            className="absolute top-1/3  max-sm:top-1/2  max-sm:w-90 left-1/2 transform -translate-x-1/2 -translate-y-1/2 font-bold text-5xl "
           >
-            <h1 className="text-3xl md:text-5xl lg:text-6xl font-bold text-white">
+            <h1 className="text-3xl md:text-5xl lg:text-6xl  font-bold text-white ">
               Bienvenue au N&T Hotel
             </h1>
             <Motion.p
               initial={{ opacity: 0 }}
               animate={{ opacity: 1 }}
               transition={{ delay: 1.5 }}
-              className="mt-2 text-2xl md:text-xl text-center text-white font-medium italic ml-5"
+              className="mt-2 text-2xl md:text-xl text-center text-white font-medium italic"
             >
               Votre séjour d'exception
             </Motion.p>
@@ -79,44 +80,45 @@ function Home() {
               stiffness: 50,
               delay: 2,
             }}
-            className="absolute bottom-50 left-1/2 -translate-x-1/2 -translate-y-1/2 text-center animate-bounce"
+            className="absolute bottom-50 max-sm:bottom-10 left-1/2 -translate-x-1/2 -translate-y-1/2 text-center animate-bounce"
           >
             <p className="text-white text-sm font-bold uppercase">Découvrir</p>
-            <div className="inline-flex border-5 p-2 rounded-full mt-5 animate-bounce">
+            <div className="inline-flex border-5 border-white p-2 rounded-full mt-5 animate-bounce">
               <ArrowDown size={20} className="text-white" />
             </div>
           </Motion.div>
         </div>
 
         <Motion.div
-          className="absolute left-1/2 -translate-x-1/2 -translate-y-1/2 text-center bg-teal-100/10 backdrop-blur-xs border-5 p-10 rounded-2xl"
+          className="relative mx-auto w-[65%] max-xl: max-xl:mb-10 max-xl:my-10 -my-25 max-sm:my-10 text-center bg-teal-100/10 backdrop-blur-xs border-5 p-10 max-sm:p-2 rounded-2xl mb-15"
           initial={{ opacity: 0, y: 50 }}
           whileInView={{ opacity: 1, y: 0 }}
           viewport={{ once: true, margin: "-50px" }}
           transition={{ duration: 1, ease: "easeOut" }}
         >
-          <div className="flex">
+          <div className="flex flex-wrap justify-center gap-y-6 max-sm:gap-y-1 md:gap-0">
             {decouvrHotel.map((dHotel, index) => (
               <Motion.li
                 key={dHotel.id || index}
                 initial={{ opacity: 0, x: -20 }}
                 whileInView={{ opacity: 1, x: 0 }}
-                viewport={{ once: true, margin: "-50px" }}
                 transition={{
                   duration: 2,
                   delay: index * 0.5,
                   ease: "easeOut",
                 }}
-                className={`list-none px-20 ${
-                  index !== 0 ? "border-l-5 border-teal-800" : ""
+                className={`list-none px-20 2xl:px-auto max-xl:border-none  ${
+                  index !== 0
+                    ? "border-l-5 border-teal-800 max-sm:border-l-0 max-sm:border-t-5 max-sm:border-teal-800 max-sm:pt-4 max-sm:mt-4"
+                    : ""
                 }`}
               >
                 <Motion.button
-                  className="w-25 h-auto cursor-pointer flex flex-col gap-y-5 items-center"
+                  className="w-25 h-auto max-sm:w-15 md:w-15 cursor-pointer flex flex-col gap-y-5 items-center"
                   whileHover={{ scale: 1.05 }}
                 >
                   <span className="text-3xl">{dHotel.logo}</span>
-                  <span className="font-bold text-2xl text-teal-900 uppercase tracking-wide">
+                  <span className="font-bold text-2xl max-sm:text-xl text-teal-900 uppercase tracking-wide">
                     {dHotel.name}
                   </span>
                 </Motion.button>
@@ -125,13 +127,6 @@ function Home() {
           </div>
         </Motion.div>
 
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
         {/* TYPE DE CHAMBRE NASA */}
         <div className="space-y-12">
           <div>
@@ -140,6 +135,9 @@ function Home() {
           <div>
             <Avantage />
           </div>
+        </div>
+        <div>
+          <OurServices />
         </div>
       </div>
     </>
---
+++ src/Pages/Home/ourService.jsx
@@ -0,0 +1,11 @@
+import Service from "../../assets/image/Service.jpg";
+
+export default function OurServices() {
+  return (
+    <>
+      <div>
+        <img src={Service} className="w-full h-auto"/>
+      </div>
+    </>
+  );
+}
