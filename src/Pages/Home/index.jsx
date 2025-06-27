691411ea1eef3cddcb40370f0af3c5a2655cebd8
Njaka02 <njakaharilantoandriamijeri@gmail.com>
2025-06-27 18:20:56 +0300

    [FIX] fix bug

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -6,19 +6,18 @@ import { ArrowDown } from "lucide-react";
 import TypeDeChambre from "./typeDeChambre";
 import { useNavigate } from "react-router-dom";
 import { motion as Motion } from "framer-motion";
-import home1 from "../../assets/image/sunset.jpg";
-import SPA from "../../assets/image/beachSpa.jpg";
-import Bar from "../../assets/image/beachBar.jpg";
-import NTLogo from "../../assets/Logo/N_TLogo.png";
-import BarLogo from "../../assets/Logo/BARLogo.png";
-import SpaLogo from "../../assets/Logo/SPALogo.png";
-import Chambre from "../../assets/image/beachRoom.jpg";
-import ChambreLogo from "../../assets/Logo/ChambreLogo.png";
-import Restaurant from "../../assets/image/beachRestaurant.jpg";
-import RestaurantLogo from "../../assets/Logo/RestaurantLogo.png";
+import home1 from "../../assets/image/Login.jpg";
+import Chambre from "../../assets/image/hotel.png";
+import Restaurent from "../../assets/image/restaurant.png";
+import NTLogo from "../../assets/image/N_TLogo-removebg-preview.png";
 
 function Home() {
-  const navigate = useNavigate();
+  const decouvrHotel = [
+    { name: "Chambre", path: "/", logo: <img src={Chambre} /> },
+    { name: "Restaurant", path: "/", logo: <img src={Restaurent} /> },
+    { name: "Bar", path: "/", logo: <img src={Bar} /> },
+    { name: "Spa", path: "/", logo: <img src={SPA} /> },
+  ];
   return (
     <>
       <div className="relative">
@@ -99,61 +98,35 @@ function Home() {
           transition={{ duration: 1, ease: "easeOut" }}
           className="grid grid-cols-4 max-xl:grid-cols-2 max-sm:grid-cols-1 gap-x-10 max-xl:gap-x-5 relative -mt-60 max-md:-mt-0 max-lg:-mt-0 max-sm:-mt-0 z-10 mb-60 max-xl:mb-0 mx-20 max-xl:mx-10 max-sm:mx-5 max-sm:mb-10"
         >
-          {[
-            {
-              title: "Chambre",
-              image: Chambre,
-              logo: ChambreLogo,
-              path: "/ChambreType",
-              text: " Découvrez le confort raffiné de notre chambre, alliant élégance, sérénité et services haut de gamme.",
-            },
-            {
-              title: "Bar",
-              image: Bar,
-              logo: BarLogo,
-              path: "/Bar",
-              text: "Dégustez nos cocktails artisanaux dans un cadre chaleureux, au rythme de la musique malagasy et lounge.",
-            },
-            {
-              title: "Restaurant",
-              image: Restaurant,
-              logo: RestaurantLogo,
-              path: "/Restaurant",
-              text: "  Des mets traditionnels revisités avec finesse, dans un cadre chaleureux et raffiné.",
-            },
-            {
-              title: "Spa",
-              image: SPA,
-              logo: SpaLogo,
-              path: "/Spa",
-              text: " Laissez-vous envelopper par les soins traditionnels aux huiles naturelles locales",
-            },
-          ].map((item, index) => (
-            <Motion.div
-              initial={{ opacity: 0, x: 50 }}
-              whileInView={{ opacity: 1, x: 0 }}
-              viewport={{ margin: "-100px" }}
-              transition={{ duration: 2, ease: "easeOut" }}
-              key={index}
-              onClick={() => navigate(item.path)}
-              className="relative cursor-pointer bg-white/50 max-sm:bg-transparent p-5 max-xl:mb-60 max-md:mb-40 max-lg:mb-40 max-sm:mb-15"
-            >
-              <img
-                src={item.image}
-                alt=""
-                className="w-full h-65 mx-auto hover:brightness-70 hover:contrast-125 transition-all duration-300 max-sm:hidden"
-              />
-              <div className="absolute top-1/2 max-md:top-2/5 left-1/2 transform -translate-x-1/2 max-sm:-translate-x-3/5 sm:-translate-x-1/2 -translate-y-1 flex flex-col max-sm:flex-row max-sm:gap-x-5 items-center bg-white/90 z-20 p-5 max-sm:p-0 w-[calc(100%-5rem)]">
-                <img src={item.logo} alt="" className="w-30 max-sm:w-20" />
-                <h1 className="uppercase mt-5 font-bold text-2xl text-[#D2A76F]">
-                  {item.title}
-                </h1>
-                <p className="text-center opacity-80 mt-5 max-sm:hidden">
-                  {item.text}
-                </p>
-              </div>
-            </Motion.div>
-          ))}
+          <div className="flex flex-wrap justify-center gap-y-6 max-sm:gap-y-1 md:gap-0">
+            {decouvrHotel.map((dHotel, index) => (
+              <Motion.li
+                key={dHotel.id || index}
+                initial={{ opacity: 0, x: -20 }}
+                whileInView={{ opacity: 1, x: 0 }}
+                transition={{
+                  duration: 1,
+                  delay: index * 0.5,
+                  ease: "easeOut",
+                }}
+                className={`list-none px-20 2xl:px-auto max-xl:border-none ${
+                  index !== 0
+                    ? "border-l-5 border-teal-800 max-sm:border-l-0 max-2xl:border-l-0 max-sm:border-t-5 max-sm:border-teal-800 max-sm:pt-4 max-sm:mt-4"
+                    : ""
+                }`}
+              >
+                <Motion.button
+                  className="w-25 h-auto max-sm:w-15 md:w-15 cursor-pointer flex flex-col gap-y-5 items-center"
+                  whileHover={{ scale: 1.05 }}
+                >
+                  <span className="text-3xl">{dHotel.logo}</span>
+                  <span className="font-bold text-2xl max-sm:text-xl text-teal-900 uppercase tracking-wide">
+                    {dHotel.name}
+                  </span>
+                </Motion.button>
+              </Motion.li>
+            ))}
+          </div>
         </Motion.div>
 
         {/* TYPE DE CHAMBRE NASA */}
